ba6bcc4766b6459a28c632f4f78b23043bb47046
Mab <mabchhan@gmail.com>
2023-03-15 16:24:26 -0700

    Added manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,6 +29,26 @@ module.exports = () => {
         swSrc: "./src/sw.js",
         swDest: "service-worker.js",
       }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Never forget your contacts!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
@@ -44,6 +64,10 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
